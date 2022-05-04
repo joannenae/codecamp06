6f6aa0018a3470c64f70bb7c3c4c37f87611b824
joannenae <joannenaedevelop@gmail.com>
2022-05-04 12:40:04 +0900

    update ssr

---
+++ class_build/pages/boards/[boardid]/index.js
@@ -0,0 +1,54 @@
+import { useRouter } from "next/router";
+import Head from "next/head";
+import { gql, request } from "graphql-request";
+
+export default function BoardDetailPage() {
+  const router = useRouter();
+
+  return (
+    <div>
+      <Head>
+        <meta property="og:title" content={props.myboardData?.title} />
+        <meta property="og:description" content={props.myboardData?.contents} />
+        <meta property="og:image" content={props.myboardData?.images[0]} />
+      </Head>
+      <div>
+        안녕하세요? 게시글 상세페이지 입니다 !! 게시글 id는{" "}
+        {router.query.boardid}
+      </div>
+    </div>
+  );
+}
+
+const FETCH_BOARD = gql`
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      title
+      contents
+      images
+    }
+  }
+`;
+
+//  이 페이지는 서버사이드렌더링 할랭
+export const getServerSideProps = async (context) => {
+  // 데이터를 요청할 것
+  // const { data } = useQuery(FETCH_BOARD) - 이건 안됨
+  const result = await request(
+    "https://backend06.codebootcamp.co.kr/graphql",
+    FETCH_BOARD,
+    {
+      boardid: context.query.boardid,
+    }
+  );
+
+  return {
+    props: {
+      myboardData: {
+        title: result.fetchBoard.title,
+        contents: result.fetchBoard.contents,
+        images: result.fetchBoard.images,
+      },
+    },
+  };
+};
